99f4150f169bb193dfd7343615997ae01ea7575e
Arthur <arthur_bandaryk@mail.ru>
2023-04-06 20:52:32 +0300

    Build SDL and link dynamically

--- 02-homework-sdl-lib/main.cpp
+++ 02-homework-sdl-lib/main.cpp
@@ -1,8 +1,33 @@
+#include <SDL3/SDL.h> // For SDL_GetError().
+#include <SDL3/SDL_version.h>
+
+//
 #include <glog/logging.h>
+
+//
 #include <iostream>
+
+///////////////////////////////////////////////////////////////////////////////
 
 // All glog logs will be written to stderr.
 #define ARCI_LOGGING_TO_STDERR
+
+std::ostream& operator<<(std::ostream& os, const SDL_version& version) {
+  os << static_cast<int>(version.major) << "."
+     << static_cast<int>(version.minor) << "."
+     << static_cast<int>(version.patch);
+  return os;
+}
+
+bool operator==(
+    const SDL_version& compiled_version,
+    const SDL_version& linked_version) {
+  return compiled_version.major == linked_version.major
+      && compiled_version.minor == linked_version.minor
+      && compiled_version.patch == linked_version.patch;
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 int main(int, char** argv) {
 // IMPORTANT NOTE: this global flag should be defined before
@@ -23,9 +48,28 @@ int main(int, char** argv) {
   // Initialize Google's logging library.
   google::InitGoogleLogging(argv[0]);
 
-  LOG(INFO) << "Hello log from `glog` library";
+  SDL_version compiled_sdl_version{}, linked_sdl_version{};
 
-  std::cout << "Hello, SDL!" << std::endl;
+  SDL_VERSION(&compiled_sdl_version);
 
-  return std::cout.fail() ? EXIT_FAILURE : EXIT_SUCCESS;
+  CHECK_EQ(SDL_GetVersion(&linked_sdl_version), 0)
+      << "SDL_GetError() failed with the following error: "
+      << SDL_GetError();
+
+  std::cout << "SDL compiled version: "
+            << compiled_sdl_version
+            << std::endl;
+
+  std::cout << "SDL linked version: "
+            << linked_sdl_version
+            << std::endl;
+
+  CHECK_EQ(compiled_sdl_version, linked_sdl_version)
+      << "Oops, SDL compiled version and link version differ";
+
+  return std::cout.fail()
+      ? EXIT_FAILURE
+      : EXIT_SUCCESS;
 }
+
+///////////////////////////////////////////////////////////////////////////////
