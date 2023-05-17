a0dc2a48087f19c4cc2c92686426c35ae580e9b9
Arthur <arthur_bandaryk@mail.ru>
2023-05-17 15:13:21 +0300

    Add glm library and prepare folder for math tasks

---
+++ 06-homework-opengl-texture-and-morphing/math-on-cpu/main.cxx
@@ -0,0 +1,69 @@
+#include "engine.hxx"
+
+//
+#include <glog/logging.h>
+
+//
+#include <memory>
+
+int main(int, char** argv)
+{
+    FLAGS_logtostderr = true;
+    google::InitGoogleLogging(argv[0]);
+
+    // Create engine.
+    std::unique_ptr<arci::iengine, void (*)(arci::iengine*)> engine {
+        arci::engine_create(),
+        arci::engine_destroy
+    };
+
+    engine->init();
+
+    arci::triangle triangle_low {
+        { -0.5f, -0.5f, 0.f, 0.f, 0.f, 0.f, 0.f, 1.f },
+        { 0.5f, -0.5f, 0.f, 0.f, 0.f, 0.f, 1.f, 1.f },
+        { -0.5f, 0.5f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f },
+    };
+
+    arci::triangle triangle_high {
+        { -0.5f, 0.5f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f },
+        { 0.5f, 0.5f, 0.f, 0.f, 0.f, 0.f, 1.f, 0.f },
+        { 0.5f, -0.5f, 0.f, 0.f, 0.f, 0.f, 1.f, 1.f },
+    };
+
+    std::unique_ptr<arci::itexture> texture {
+        engine->create_texture("worm.png")
+    };
+
+    bool loop_continue { true };
+
+    while (loop_continue)
+    {
+        arci::event event {};
+
+        while (engine->process_input(event))
+        {
+            if (event.is_quitting)
+            {
+                loop_continue = false;
+                break;
+            }
+            else
+            {
+                if (event.keyboard_info)
+                {
+                    // Do nothing for now.
+                }
+            }
+        }
+
+        engine->render(triangle_low, texture.get());
+        engine->render(triangle_high, texture.get());
+
+        engine->swap_buffers();
+    }
+
+    engine->uninit();
+
+    return EXIT_SUCCESS;
+}
